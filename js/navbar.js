e0d6c80c8884dc8635eb930ff5729a3f21ecc415
Moh Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2026-06-12 19:25:36 +0800

    update beberapa feature
    - menambah animasi di image hero section
    - update navbar: auto close ketika kelik menu dan ubah icon humbueger menu

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const checkBox = document.querySelector("nav .nav-container .button-menu #button-menu")
-    const listMenu = document.querySelector("nav .nav-container ul")
-    if(checkBox instanceof HTMLInputElement) {
+    const checkBox = document.querySelector("nav .nav-container .button-menu #button-menu");
+    const listMenu = document.querySelector("nav .nav-container ul");
+    const menuLinks = document.querySelectorAll("nav .nav-container ul li a");
+
+    if (checkBox instanceof HTMLInputElement && listMenu instanceof HTMLUListElement) {
         checkBox.addEventListener('change', () => {
-            checkBox.checked ? listMenu.classList.add("show") : listMenu.classList.remove("show")
-        })
+            checkBox.checked ? listMenu.classList.add("show") : listMenu.classList.remove("show");
+        });
+
+        menuLinks.forEach((link) => {
+            link.addEventListener("click", () => {
+                checkBox.checked = false;
+                listMenu.classList.remove("show");
+            });
+        });
     }
-})
+});
